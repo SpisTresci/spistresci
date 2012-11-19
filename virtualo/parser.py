c5d013b9802e68f085b56900c17b27670f91f3c7
Krzysztof Szumny <kszumny@spistresci.pl>
2012-11-19 21:11:28 +0100

    T25: Fix for invalid descriptionShort tag

--- virtualo/parser.py
+++ virtualo/parser.py
@@ -24,17 +24,41 @@ def getTagExpliciteValue(product, tagName, default=""):
 
 
 
+def removeDescriptionShortTag(filename):
+    """
+    This is done because of bug T25. DescriptionShort tags very often contain within itself unclosed formating tags.
+    Because information which is in descriptionShort is redundand (we have to keep full description anyway), so we can remove this tag from xmls.
+    """
+
+    ifile = open(filename,'r',)
+    text = ifile.read()
+    ifile.close()
+
+    text = re.sub(r'(<descriptionShort>.*?</descriptionShort>)',r'', text, 0, re.DOTALL| re.MULTILINE | re.VERBOSE)
+
+    file_times = open(filename,"w")
+    file_times.write(text)
+    file_times.close()
+
+
+
 def main():
 
 
-    i=4
+    i=1
 
-    while os.path.exists('cennik_full' + os.sep + 'VirtualoProducts' + str(i) +'.xml') == True:
+    while True:
 
-        DOMTree = minidom.parse('cennik_full' + os.sep + 'VirtualoProducts' + str(i) +'.xml')
+        filename  = 'cennik_full' + os.sep + 'VirtualoProducts' + str(i) +'.xml'
+        if not os.path.exists(filename):
+            break
+
+        removeDescriptionShortTag(filename)
+        DOMTree = minidom.parse(filename)
         #print DOMTree.toxml()
 
         products = DOMTree.childNodes.item(0).childNodes
+
 
         print "START =======" + 'VirtualoProducts' + str(i) +'.xml' + "=========="
 
@@ -55,7 +79,7 @@ def main():
 
             url = getTagValue(product, 'url')
             description = getTagExpliciteValue(product, 'description')
-            descriptionShort = getTagValue(product, 'descriptionShort')
+            #descriptionShort = getTagValue(product, 'descriptionShort')
             rating = getTagValue(product, 'rating')
 
             #http://virtualo.pl/kodeks_karny_kodeks_postepowania_karnego_kodeks_karny/i123959/
@@ -70,9 +94,11 @@ def main():
 
 
             #format = product.getElementsByTagName('format')[0].firstChild.nodeValue
+
             #security = product.getElementsByTagName('security')[0].firstChild.nodeValue
 
         print "END   =======" + 'VirtualoProducts' + str(i) +'.xml' + "=========="
+        print "odczytano " + str(len(products)) + " produktow"
         i=i+1
 
     pass
