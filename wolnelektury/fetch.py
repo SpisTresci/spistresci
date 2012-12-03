dcaf4ce94471f0c4a1244c7902adeba532dc951c
Piotr Zawislak <pzawislak@opera.com>
2012-12-03 11:27:58 +0100

    "T28 konektor wolnelektury, small fix"

--- wolnelektury/fetch.py
+++ wolnelektury/fetch.py
@@ -4,20 +4,19 @@ import json
 url = 'http://wolnelektury.pl/api/books/'
 
 def main():
-    page = urllib2.urlopen(url)
-    books_page = page.read()
+    socket = urllib2.urlopen(url)
+    books_page = socket.read()
     book_list = json.loads(books_page)
     for book in book_list:
-        book_page = urllib2.urlopen(book[u'href'])
+        book_socket = urllib2.urlopen(book[u'href'])
+        book_page =book_socket.read()
+        book_socket.close()
+        print book_page;
         book_details = json.loads(book_page)
-        book = dict((n,book.get(n,0),book_details.get(n,0)) for n in set(book)|set(book_details))
+        (book.setdefault(n,book_details.get(n,0)) for n in set(book_details))
 
-#    print books;
-
-
-    
+    print books;
 
 
 if __name__ == '__main__':
     main()
-
