bd720322fdcf8aa37d74cb058b4d42bec831b7ef
Krzysztof Szumny <kszumny@opera.com>
2012-12-19 21:08:12 +0100

    T45: zmiana minidom.xml.dom na ElementTree.xml.etree

--- dobryebook/DobryEbook.py
+++ dobryebook/DobryEbook.py
@@ -1,42 +1,39 @@
 from generic.XMLConnector import XMLConnector
 import os
-from xml.dom import minidom
+from xml.etree import ElementTree as et
 
 class DobryEbook(XMLConnector):
-       
-    
+
     url = "http://dobryebook.pl/xml/spistresci.xml"
-    
+
     def test(self):
         print "test"
-        
+
     def __init__(self):
         XMLConnector.__init__(self, self.url, XMLConnector.SINGLE_XML)
-        
+
     def parse(self):
-        
+
         filename  = 'spistresci.xml'
         if not os.path.exists(filename):
             exit(-1)
-        
-        DOMTree = minidom.parse(filename)
-        ebooks = DOMTree.childNodes.item(0).childNodes
-        
-        for ebook in ebooks:
-            title = self.getTagValue(ebook, 'tytul')
-            subtitle = self.getTagValue(ebook, 'podtytul')
-            author = self.getTagValue(ebook, 'autor')
-            link = self.getTagValue(ebook, 'link')
-            price = self.getTagValue(ebook, 'cena')
-            file_size = self.getTagValue(ebook, 'wielkosc_pliku')
-            numberOfPages = self.getTagValue(ebook, 'liczba_stron')
-            isbn = self.getTagValue(ebook, 'isbn')
-            smallCover = self.getTagValue(ebook, 'male_zdjecie')
-            bigCover = self.getTagValue(ebook, 'duze_zdjecie')
-            
+
+        root = et.parse(filename).getroot()
+
+        for ebook in root:
+            title = ebook.find('tytul').text
+            subtitle = ebook.find('podtytul').text
+            author = ebook.find('autor').text
+            link = ebook.find('link').text
+            price = ebook.find('cena').text
+            file_size = ebook.find('wielkosc_pliku').text
+            numberOfPages = ebook.find('liczba_stron').text
+            isbn = ebook.find('isbn').text
+            smallCover = ebook.find('male_zdjecie').text
+            bigCover = ebook.find('duze_zdjecie').text
+
             print "Tytul: " + title
             print "Autor: " + author
             print "Cena: " + price
             print "ISBN: " + isbn + "\n"
-            
-            
+
