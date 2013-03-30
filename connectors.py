91d47e8de079199e30c56fef24019fcafcb54670
Piotr Zawislak <pzawislak@spistresci.pl>
2013-03-30 01:12:21 +0100

    backup.py and connectors.py is now the same file

--- connectors.py
+++ connectors.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 import ConfigParser
 import sys
+import os
 
 from utils import logger_instance
 from connectors import Tools
 from connectors.generic import *
 from sqlwrapper import *
 
+
+def run_connectors(connector):
+    connector.fetchData()
+    connector.applyFilters()
+    connector.parse()
+
+def run_backup(connector):
+    #only download, do not unpack
+    connector.fetchData(unpack=False)
+
+def magically_run(app_name):
+    return getattr(sys.modules[__name__], 'run_%s' % app_name)
+
 def main():
 
+    base_name = os.path.basename(sys.argv[0])
+    app_name = os.path.splitext(base_name)[0]
+    conf_name  = '%s.ini' % app_name
+    GenericConnector.config_file = os.path.join('conf', conf_name)
     GenericConnector.read_config()
     Logger = logger_instance(GenericConnector.config_object.get('DEFAULT', 'log_config'))
 
     connector_classnames = Tools.get_classnames()
     final_connector_dic = Tools.filter_classnames(connector_classnames, sys.argv[1:])
 
-    SqlWrapper.init(GenericConnector.config_object.get('DEFAULT', 'db_config'), connectors=final_connector_dic.keys())
+    if app_name != 'backup':
+        SqlWrapper.init(GenericConnector.config_object.get('DEFAULT', 'db_config'), connectors=final_connector_dic.keys())
     connectors = [ Tools.load_connector(classname=connector[1], config=GenericConnector.config_object)
                    (name=connector[0])
                    for connector in final_connector_dic.items() ]
@@ -24,13 +43,10 @@ def main():
 
     for connector in connectors:
         try:
-            connector.fetchData()
-            connector.applyFilters()
-            connector.parse()
+            magically_run(app_name)(connector)
         except Exception:
-            Logger.exception('Error executing connector %s' % connector.name)
+            Logger.exception('Error executing %s, in connector %s' % (app_name, connector.name) )
     Logger.debug('Execution finished')
-
 
 if __name__ == '__main__':
     main()
