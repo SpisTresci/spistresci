bf8e74487fcd85ffd5363f0e9a278a55214e8cfc
Krzysztof Szumny <kszumny@spistresci.pl>
2013-04-28 17:57:35 +0200

    T111, T173, T212, T145: Zinamon Pack: new makeDict, import2db mechanism, unittests, new parse

--- connectors/specific/Zinamon.py
+++ connectors/specific/Zinamon.py
@@ -1,33 +1,47 @@
-from connectors.generic import XMLConnector
+from connectors.generic import *
 import lxml.etree as et
-import urllib2
-import os
+from sqlwrapper import *
 
 class Zinamon(XMLConnector):
 
     #dict of xml_tag -> db_column_name translations
     xml_tag_dict = {
-        'tytul':'title',
-        'url':'url',
-        'autorzy':'author',
-        'format':'format',
-        'okladka':'cover',
-        'cena':'price',
-        'id':'id',
-        'isbn':'isbn',
-        'kategorie':'category',
-        'zabezpieczenia':'protection',
+        './tytul':('title', ''),
+        './url':('url', ''),
+        './autorzy':('authors', ''),
+        './format':('format', ''),
+        './okladka':('cover', ''),
+        './cena':('price', ''),
+        './id':('external_id', ''),
+        './isbn':('isbn', ''),
+        './kategorie':('category',''),
+        './zabezpieczenia':('protection',''),
     }
 
-    def parse(self):
-        filename = os.path.join(self.backup_dir, self.filename)
-        root = et.parse(filename).getroot()
-        offers = list(root)
-        if self.limit_books:
-            offers = offers[:self.limit_books]
-        for book in offers:
-            dic = self.make_dict(book)
-            print dic
+Base = SqlWrapper.getBaseClass()
 
+class ZinamonBook(GenericBook, Base):
+    id = Column(Integer, primary_key=True)
 
+    title = Column(Unicode(265))        #207
+    url = Column(Unicode(512))          #338
+    #authors
+    format = Column(Unicode(8))         #4
+    cover = Column(Unicode(64))         #38
+    price = Column(Integer)             #grosze
+    external_id = Column(Unicode(16))   #10
+    isbn = Column(Unicode(13))          #0
+    category = Column(Unicode(512))     #20
+    protection = Column(Unicode(16))    #9
 
+class ZinamonBookDescription(GenericBookDescription, Base):
+    pass
+
+class ZinamonAuthor(GenericAuthor, Base):
+    pass
+
+class ZinamonBookPrice(GenericBookPrice, Base):
+    pass
+
+class ZinamonBooksAuthors(GenericBooksAuthors, Base):
+    pass
