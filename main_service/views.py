17a95abf3e91cb16dd2e22c758e60e94dd892fae
Krzysztof Szumny <kszumny@spistresci.pl>
2013-05-27 16:52:20 +0200

    - New filter module is added: "Serwisy" - filters module is extracted to search_filters.html - Results is extracted to search_results_list.html

--- main_service/views.py
+++ main_service/views.py
@@ -12,4 +12,10 @@ def index(request):
     return render_to_response('index.html')
 
 def search(request):
-    return render_to_response('search.html')
+    
+    dic = {}
+    servise_names = ["abooki.pl", "albertus.pl", "audeo.pl", "audiobook.pl", "audioteka.pl", "barbelo.com.pl", "bezkartek.pl", "bezokladki.pl", "booki25.pl", "bookio.pl", "bookmaster.pl", "bookoteka.pl", "bookson.pl", "cdp.pl", "czarty.pl", "sklep.czatroom.pl", "czytajtanio.pl", "czeskieklimaty.pl", "czytam.pl", "dobryebook.pl", "ekiosk.pl", "eporadniki.pl", "etekst.pl", "ebooki24.pl", "ebook.memento.pl", "ebook.pl", "ebooki123.pl", "ebooki.allegro.pl", "ebooki.orange.pl", "ebooki.tmobile.pl", "ebookomania.pl", "eBookpoint.pl", "helion.pl", "onepress.pl", "sensus.pl", "septem.pl", "ebookowo.pl", "ebookowo.pl", "ebooks43.pl", "eclicto.pl", "empik.pl", "escapemagazine.pl", "fabryka.pl", "ksiazki.pl", "kodeksywmp3.pl", "fantastykapolska.pl", "gandalf.com.pl", "gutenberg.org", "iBook.net.pl" ]
+    services = zip(range(len(servise_names)), servise_names)
+    dic["services"] = services
+    dic["prefix"] = "s"
+    return render_to_response('search.html', dic)
