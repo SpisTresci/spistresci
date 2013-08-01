5f3e8ec72fa2d78421a46dc718aa92b33e6b9fd5
Krzysztof Szumny <kszumny@spistresci.pl>
2013-08-01 23:27:23 +0200

    T358: Production settings for eGazeciarz authentication.

--- spistresci/settings_production.py
+++ spistresci/settings_production.py
@@ -17,6 +17,15 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
+    },
+    'baza_calibre': {
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'baza_calibre',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
     }
 }
 
