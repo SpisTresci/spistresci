7561e8e9c5a35c312ff8886636a1aea6d4dffa59
Krzysztof Szumny <kszumny@spistresci.pl>
2013-08-01 22:44:39 +0200

    T358: Full support of authentication by eGazeciarz credentials.

--- spistresci/settings.py
+++ spistresci/settings.py
@@ -18,8 +18,21 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
+    },
+    'baza_calibre': {
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'baza_calibre',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
     }
 }
+
+DATABASE_ROUTERS = [
+    'spistresci.db_routers.DBRouter',
+]
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
