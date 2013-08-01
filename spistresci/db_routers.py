a2c018845d1c3740fc6c860795c55960364735ba
Krzysztof Szumny <kszumny@spistresci.pl>
2013-08-01 22:44:39 +0200

    T358: Full support of authentication by eGazeciarz credentials.

---
+++ spistresci/db_routers.py
@@ -0,0 +1,12 @@
+class DBRouter(object):
+    def db_for_read(self, model, **hints):
+        if model._meta.object_name == 'eGazeciarzUser':
+            return 'baza_calibre'
+        else:
+            return 'default'
+
+    def db_for_write(self, model, **hints):
+        if model._meta.object_name == 'eGazeciarzUser':
+            return 'baza_calibre'
+        else:
+            return 'default'
--- spistresci/models.py
+++ spistresci/models.py
@@ -14,6 +14,11 @@ class BookstoreSolrWrapper(models.Model):
 
 
 class eGazeciarzUser(models.Model):
+    '''
+    Reimplement AbstractBaseUser, because it cannot inherit it
+    since overriding model fields is not possible in django
+    at the moment.
+    '''
     USERNAME_FIELD = 'username'
 
     id = models.IntegerField(primary_key=True, unique=True, db_index=True)
@@ -34,8 +39,58 @@ class eGazeciarzUser(models.Model):
     last_login = models.DateTimeField(db_column='lastvisitDate', default=timezone.now)
 
     class Meta:
-        db_table = 'eGazeciarzUser'
+        db_table = 'jos_users'
 
     is_active = True
 
     REQUIRED_FIELDS = []
+
+    def get_username(self):
+        "Return the identifying username for this User"
+        return getattr(self, self.USERNAME_FIELD)
+
+    def __str__(self):
+        return self.get_username()
+
+    def natural_key(self):
+        return (self.get_username(),)
+
+    def is_anonymous(self):
+        """
+        Always returns False. This is a way of comparing User objects to
+        anonymous users.
+        """
+        return False
+
+    def is_authenticated(self):
+        """
+        Always return True. This is a way to tell if the user has been
+        authenticated in templates.
+        """
+        return True
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        """
+        Returns a boolean of whether the raw_password was correct. Handles
+        hashing formats behind the scenes.
+        """
+        def setter(raw_password):
+            self.set_password(raw_password)
+            self.save(update_fields=["password"])
+        return check_password(raw_password, self.password, setter)
+
+    def set_unusable_password(self):
+        # Sets a value that will never be a valid hash
+        self.password = make_password(None)
+
+    def has_usable_password(self):
+        return is_password_usable(self.password)
+
+    def get_full_name(self):
+        raise NotImplementedError()
+
+    def get_short_name(self):
+        raise NotImplementedError()
