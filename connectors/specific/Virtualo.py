a662985b22ebffe9e946bacca1ca843d50da3c87
Krzysztof Szumny <kszumny@spistresci.pl>
2013-09-01 18:10:31 +0200

    Disabling Virtualo Filter. CDATA was added by Virtualo.

--- connectors/specific/Virtualo.py
+++ connectors/specific/Virtualo.py
@@ -7,12 +7,12 @@ class Virtualo(XMLConnector):
     depth = 0
     #dict of xml_tag -> db_column_name translations
     xml_tag_dict = {
-        'external_id': ('./coverId', ''),
         'title' : ('./title', ''),
         'formats' : ('./format', ''),
         'protection': ('./security', ''),
         'price' : ('./price', 0),
         'isbns' : ('./isbn', ''),
+        'cover': ('./coverId', ''),
         'authors' : ('./authors', ''),
         'url' : ('./url', ''),
         'description' : ('./description', ''),
@@ -37,7 +37,11 @@ class Virtualo(XMLConnector):
             dic[rating_tag] = rating_normalized
 
     def adjust_parse(self, dic):
-        dic["cover"] = "http://static.virtualo.pl/media_images/normal/" + dic["external_id"] + ".jpg"
+        self.create_id_from_url(dic)
+        dic["cover"] = "http://static.virtualo.pl/media_images/normal/" + dic["cover"] + ".jpg"
+
+    def create_id_from_url(self, dic):
+        dic['external_id'] = dic['url'].split('/')[4][1:]
 
 Base = SqlWrapper.getBaseClass()
 
