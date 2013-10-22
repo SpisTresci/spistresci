8049d6b119afa567dcf47135463866a4be02fe53
Krzysztof Szumny <kszumny@spistresci.pl>
2013-10-22 21:59:40 +0200

    New links to latest solr database versions.
    Conflicts:
    	settings_production.py

--- settings_production.py
+++ settings_production.py
@@ -43,7 +43,7 @@ DATABASES = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://solr1.spistresci.pl:8090/solr1/masterbook_alpha_10',
+        'URL': 'http://solr1.spistresci.pl:8090/solr1/masterbook_latest',
         'EXCLUDED_INDEXES': [
             'main_service.search_indexes.BookstoreIndex',
         ]
@@ -51,7 +51,7 @@ HAYSTACK_CONNECTIONS = {
 
     'bookstore': {
         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://solr1.spistresci.pl:8090/solr1/bookstore_alpha_10',
+        'URL': 'http://solr1.spistresci.pl:8090/solr1/bookstore_latest',
         'EXCLUDED_INDEXES': [
             'main_service.search_indexes.MasterBookIndex',
         ]
