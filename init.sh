8011b39df8f200fcf18b7329f588fe2547e77e7b
Krzysztof Szumny <kszumny@spistresci.pl>
2013-10-30 03:25:34 +0100

    Init script.

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install $(grep -vE "^\s*#" requirements.apt  | tr "\n" " ")
+
+git clone git://gitslave.git.sourceforge.net/gitroot/gitslave/gitslave /tmp/gitslave
+cd /tmp/gitslave/
+sudo make install -C contrib
+cd -
+
+gits fetch
+gits populate
+
+find . -name "requirements.pip" -exec pip install -r {} \;
+
