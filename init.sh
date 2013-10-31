05e0fd6d56aa911033da5e78f8f62d8e383ebe4f
Krzysztof Szumny <kszumny@spistresci.pl>
2013-10-31 21:33:33 +0100

    Add missing command in init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ sudo apt-get install $(grep -vE "^\s*#" requirements.apt  | tr "\n" " ")
 
 git clone git://gitslave.git.sourceforge.net/gitroot/gitslave/gitslave /tmp/gitslave
 cd /tmp/gitslave/
+sudo make install
 sudo make install -C contrib
 cd -
 
