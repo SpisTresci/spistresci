6aa7ef2531b7101af3971e2f52023b61cbfa4eeb
Piotr Zawislak <pzawislak@spistresci.pl>
2013-12-13 00:33:24 +0100

    T415 WIP, Ksiazki.pl and Fabryka.pl with tests, still bug in adjust_parse

--- connectors/common/__init__.py
+++ connectors/common/__init__.py
@@ -3,3 +3,4 @@ from Ceneo import Ceneo
 from FormatInTitleConnector import FormatInTitleConnector
 from HelionBase import HelionBase
 from HelionBase import HelionBaseBook
+from KsiazkiFabrykaBase import KsiazkiFabrykaBase, KsiazkiFabrykaBaseBook
--- connectors/specific/Fabryka.py
+++ connectors/specific/Fabryka.py
@@ -1,50 +1,12 @@
-from connectors.common import Ceneo
 from sqlwrapper import *
-from connectors.generic import GenericBook
+from connectors.common import KsiazkiFabrykaBase, KsiazkiFabrykaBaseBook
 
 Base = SqlWrapper.getBaseClass()
-class Fabryka(Ceneo):
-
-    #dict of xml_tag -> db_column_name translations
-    xml_tag_dict = {
-        'external_id': ('@id', ''),
-        'price': ('@price', 0),
-        'url': ('@url', ''),
-        'availability': ('@avail', ''),
-        'category': ('./cat', ''),
-        'title': ('./name', ''),
-        'raw_title': ('./name', ''),
-        'cover': ('./imgs/main/@url', ''),
-        'description': ('./desc', ''),
-        'isbns': ("./attrs/a[@name='ISBN']", ''),
-        'publisher': ("./attrs/a[@name='Wydawnictwo']", ''),
-        'formats': ("./attrs/a[@name='Format']", ''),
-        'authors':("./attrs/a[@name='Autor']", ''),
-        'publisher':("./attrs/a[@name='Wydawnictwo']", ''),
-        'page_count': ("./attrs/a[@name='Ilosc_stron']", ''),
-        'date': ("./attrs/a[@name='Rok_wydania']", ''),
-        'sample': ("./attrs/a[@name='Fragment']", ''),
-
-    }
-
-    def adjust_parse(self, dic):
-        title = dic['title']
-        if ' [e-book]' in title:
-            title = title.replace(' [e-book]','')
-            dic['title'] = title
+class Fabryka(KsiazkiFabrykaBase):
+    title_suffix= ' [e-book]'
 
 #{'raw_title': 150, 'description': 17690, 'price': 6, 'page_count': 4, 'sample': 39, 'authors': 228, 'date': 4, 'availability': 1, 'category': 70, 'publisher': 25, 'title': 150, 'url': 208, 'cover': 112, 'isbns': 17, 'formats': 13, 'external_id': 11}
 
-class FabrykaBook(GenericBook, Base):
-    id = Column(Integer, primary_key = True)
-    category = Column(Unicode(128))      #70
-    title = Column(Unicode(256))        #150
-    raw_title = Column(Unicode(256))        #150
+class FabrykaBook(KsiazkiFabrykaBaseBook, Base):
     url = Column(Unicode(512))           #208
     cover = Column(Unicode(256))        #112
-    price = Column(Integer)
-    availability = Column(Boolean)
-    publisher = Column(Unicode(64))     #25
-    page_count = Column(Integer)
-    date = Column(Date)                 #
-    sample = Column(Unicode(64))        #39
