ce83122bf9c6c05ac3d4bf38f555ca81576d9620
Mateusz Sikora <m.sikora@skytale.pl>
2014-01-12 19:01:00 +0100

    update wykop_notifer script

--- scripts/wykop_notifier/run.py
+++ scripts/wykop_notifier/run.py
@@ -34,24 +34,37 @@ def main():
         content = f.read()
         jsn = _parse_json(content)
 
+        message_container = []
         for item in jsn:
-            for msg in item['msgs']:
-
+            timer = 0
+            for counter, msg in enumerate(item['msgs']):
                 for key, val in data.items():
                     msg = msg.replace("{%s}"%key, val)
 
-                if msg != item['msgs'][0]:
-                    time.sleep(msg.count(' '))
+                timer += msg.count(' ')
+                message_container.append((timer, item['nick'], msg, counter+1))
 
-                if not DRY_RUN:
-                    api.send_message(item['nick'], msg)
-                #else:
-                #    api.send_message("lenka21", msg)
+                # if msg != item['msgs'][0]:
+                #     time.sleep(msg.count(' '))
+                # print "%(login)s > %(nick)s: %(msg)s" % {"login":LOGIN, "nick": item['nick'], "msg":msg}
+            # print u'Użytkownik %s otrzymał wiadomość.\n' % item['nick']
 
-                print "%(login)s > %(nick)s: %(msg)s" % {"login":LOGIN, "nick": item['nick'], "msg":msg}
 
-            print u'Użytkownik %s otrzymał wiadomość.\n' % item['nick']
+    message_container = sorted(message_container, key=lambda x: x[0])
+    for index, item in enumerate(message_container):
+        timer, nick, msg, counter = item
 
+        print '%s: %s (#%s): %s' % (timer, nick, counter, msg)
+
+        if not DRY_RUN:
+            api.send_message(nick, msg)
+        #else:
+        #    api.send_message("lenka21", msg)
+
+        if index + 1 < len(message_container):
+            time_interval = message_container[index+1][0] - timer
+            if time_interval > 0:
+                time.sleep(time_interval)
 
     print u"Koniec wysyłania :)"
 
