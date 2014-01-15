92fcb7849702e98e0e04b512116910ad60642943
Mateusz Sikora <msikora@spistresci.pl>
2014-01-15 22:48:13 +0100

    add extra filters for User objects in admin panel

--- admin.py
+++ admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 from spistresci.blogger.models import BloggerProfile, BookRecommendation
 
+
+class UserAdmin(UserAdmin):
+    list_filter = UserAdmin.list_filter + ('date_joined', 'last_login')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(BloggerProfile)
 admin.site.register(BookRecommendation)
