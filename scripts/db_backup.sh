eb051851c3155199aeb9ffbd34531dee3a51b53b
Piotr Zawislak <pzawislak@spistresci.pl>
2014-02-07 02:35:17 +0100

    T529 database backup script - added date

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -7,7 +7,7 @@ export db=$2
 export host=$3
 export src=$4
 export dst=$5
-
+export date=`date +%Y%m%d_%H%M`
 mkdir -p $src 
-mysqldump -u root ${db} > ${src}/${db}.db && gzip -f ${src}/${db}.db && rsync -av -e "ssh -i ${identity}" ${src} ${host}:${dst} && rm ${src}/${db}.db.gz
+mysqldump -u root ${db} > ${src}/${db}_${date}.db && gzip -f ${src}/${db}_${date}.db && rsync -av -e "ssh -i ${identity}" ${src} ${host}:${dst} && rm ${src}/${db}_${date}.db.gz
 
