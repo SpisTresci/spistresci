838cbc221c5102b0aa2da725dd1706b853c4e0d5
Patryk Perduta <pperduta@spistresci.pl>
2014-05-16 22:18:41 +0200

    T558: reworked all stuff, all should work finally ;)

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,8 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "precise32"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-  config.vm.synced_folder "~/.ssh/", "/vagrant/.ssh/"
+  config.vm.network "private_network", type: "dhcp"
+  config.ssh.forward_agent = true
+#  config.vm.synced_folder "~/.ssh/", "/vagrant/.ssh/"
   config.vm.provision :shell, :path => "init.sh"
 end
--- init.sh
+++ init.sh
@@ -3,12 +3,15 @@
 cd /vagrant
 
 sudo apt-get -y update
+#sudo apt-get -y upgrade
 
-# Ommiting "apt-get install mysql" prompt
-#echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
-#echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
+# Ommiting prompt while installing mysql
+echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
 
 sudo apt-get -y install $(grep -vE "^\s*#" requirements.apt  | tr "\n" " ")
+
+gem install sass
 
 git clone git://gitslave.git.sourceforge.net/gitroot/gitslave/gitslave /tmp/gitslave
 cd /tmp/gitslave/
@@ -21,3 +24,4 @@ gits populate
 
 find . -name "requirements.pip" -exec sudo pip install -r {} \;
 
+/vagrant/run_tools.sh
