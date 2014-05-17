8cf9e7ec13a53fac710a5ece6e241bb1ad936524
Patryk Perduta <pperduta@spistresci.pl>
2014-05-17 13:57:50 +0200

    T558: this one doesn't work, but i have to push

--- init.sh
+++ init.sh
@@ -2,29 +2,28 @@
 
 cd /vagrant
 
-sudo apt-get -y update
+#sudo apt-get -y update
 
 # Ommiting prompt while installing mysql
-echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
-echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
+#echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
+#echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
 
-sudo apt-get -y install $(grep -vE "^\s*#" requirements.apt  | tr "\n" " ")
+#sudo apt-get -y install $(grep -vE "^\s*#" requirements.apt  | tr "\n" " ")
 
-git clone git://gitslave.git.sourceforge.net/gitroot/gitslave/gitslave /tmp/gitslave
-cd /tmp/gitslave/
-sudo make install
-sudo make install -C contrib
-cd -
+#git clone git://gitslave.git.sourceforge.net/gitroot/gitslave/gitslave /tmp/gitslave
+#cd /tmp/gitslave/
+#sudo make install
+#sudo make install -C contrib
+#cd -
 
-mkdir /root/.ssh ; touch /root/.ssh/known_hosts ; ssh-keyscan -H "dev.spistresci.pl" >> /root/.ssh/known_hosts ; chmod 600 /root/.ssh/known_hosts
-mkdir /home/vagrant/.ssh ; touch /home/vagrant/.ssh/known_hosts ; ssh-keyscan -H "dev.spistresci.pl" >> /home/vagrant/.ssh/known_hosts ; chmod 600 /home/vagrant/.ssh/known_hosts
+#mkdir /root/.ssh ; touch /root/.ssh/known_hosts ; ssh-keyscan -H "dev.spistresci.pl" >> /root/.ssh/known_hosts ; chmod 600 /root/.ssh/known_hosts
+#mkdir /home/vagrant/.ssh ; touch /home/vagrant/.ssh/known_hosts ; ssh-keyscan -H "dev.spistresci.pl" >> /home/vagrant/.ssh/known_hosts ; chmod 600 /home/vagrant/.ssh/known_hosts
 
-gits fetch
-gits populate
+#gits fetch
+#gits populate
 
-find . -name "requirements.pip" -exec sudo pip install -r {} \;
+#find . -name "requirements.pip" -exec sudo pip install -r {} \;
 
-sudo gem install sass
+sudo /opt/vagrant_ruby/bin/gem install sass
 
 su - vagrant -c "/vagrant/run_tools.sh"
-
