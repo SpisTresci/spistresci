2ddbeecd5ac3b3e4589fdf1849ccfc93b1be7b21
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2014-08-31 23:16:57 +0200

    New implementation of filters

--- spistresci/search/forms.py
+++ spistresci/search/forms.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
+
 from django import forms
-from django.forms.models import ModelChoiceIterator
-from haystack.forms import ModelSearchForm
-
-from spistresci.models import Bookstore, BookFormat, BookFormatType
-
-from django.forms.widgets import SelectMultiple, CheckboxInput, \
-    CheckboxSelectMultiple
+from django.forms.widgets import CheckboxInput, CheckboxSelectMultiple
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_text
 from django.utils.html import format_html
-from itertools import chain
+from django.utils.translation import ugettext as _
+from django.template.loader import render_to_string
+
+from haystack.forms import ModelSearchForm
+
+from spistresci.models import Bookstore, BookFormat, BookFormatType
 
 
 class STCheckboxSelectMultiple(CheckboxSelectMultiple):
@@ -20,53 +20,153 @@ class STCheckboxSelectMultiple(CheckboxSelectMultiple):
 
     def render(self, name, value, attrs=None, choices=()):
         if value is None: value = []
-        has_id = attrs and 'id' in attrs
-        final_attrs = self.build_attrs(attrs, name=name)
-        output = ['    <div class="filter_content bullet_list">']
+
         # Normalize to strings
-        str_values = set([force_text(v) for v in value])
-
-        groups = self.kwargs.get('group_by')
-
-        groups = groups.objects.all()
         model = self.choices.queryset.model
-        for j, group in enumerate(groups):
 
-            new_choices = model.objects.filter(type=group)
-            if new_choices:
-                output.append(
-                    format_html(
-                        '    <div class="filter_section{0}" >',
-                        ' filter_section_last' if j == len(groups)-1 else ''
-                    )
+        dic = {
+            'output': ['    <div class="filter_content bullet_list">'],
+            'has_id': attrs and 'id' in attrs,
+            'final_attrs': self.build_attrs(attrs, name=name),
+            'str_values': set([force_text(v) for v in value]),
+        }
+
+        groups = self.kwargs.get('group_by', [])
+        if groups:
+            groups = groups.objects.all()
+
+            for i, group in enumerate(groups):
+                dic.update({
+                    'new_choices': model.objects.filter(type=group),
+                    'last': i == len(groups)-1,
+                    'group': group,
+                    'attrs': attrs,
+                    'gr_counter': i,
+                    'name': name,
+                })
+                if dic['new_choices']:
+                    self.render_group(**dic)
+        else:
+            class gr():
+                name = "Wszystkie"
+
+            dic.update({
+                'new_choices': model.objects.all(),
+                'last': True,
+                'group': gr(),
+                'attrs': attrs,
+                'gr_counter': 0,
+                'name': name,
+            })
+            self.render_group(**dic)
+
+        dic['output'].append('</div>')
+        return mark_safe('\n'.join(dic['output']))
+
+    def render_group(self, output, new_choices, group, last, has_id,
+                     attrs, final_attrs, str_values, gr_counter, name):
+
+        output.append(
+            format_html(
+                '<div class="filter_section{0}" >',
+                ' filter_section_last' if last else ''
+            )
+        )
+        output.append(
+            format_html(
+                '<div id="menu_arrow{0}" class="filter_section_arrow"></div>',
+                group.name
+            )
+        )
+        output.append(
+            format_html(
+                '<div class="filter_section_header">{0}</div>',
+                group.name
+            )
+        )
+        output.append(format_html('<ul id="sub{0}">', gr_counter))
+
+        new_choices = [(nc.id, nc.name) for nc in new_choices]
+        for i, (value, label) in enumerate(new_choices):
+            # If an ID attribute was given, add a numeric index as a suffix,
+            # so that the checkboxes don't all have the same ID attribute.
+            if has_id:
+                final_attrs = dict(
+                    final_attrs,
+                    id='%s_%s_%s' % (attrs['id'], gr_counter, i)
                 )
-                output.append(format_html('        <div id="menu_arrow{0}" class="filter_section_arrow"></div>', group.name))
-                output.append(format_html('        <div class="filter_section_header">{0}</div>', group.name))
-                output.append(format_html('        <ul id="sub{0}">', j))
+                label_for = format_html(' for="{0}"', final_attrs['id'])
+            else:
+                label_for = ''
 
-                for i, (option_value, option_label) in enumerate([(nc.id, nc.name) for nc in new_choices]):
-                    # If an ID attribute was given, add a numeric index as a suffix,
-                    # so that the checkboxes don't all have the same ID attribute.
-                    if has_id:
-                        final_attrs = dict(final_attrs, id='%s_%s_%s' % (attrs['id'], j, i))
-                        label_for = format_html(' for="{0}"', final_attrs['id'])
-                    else:
-                        label_for = ''
+            cb = CheckboxInput(
+                final_attrs,
+                check_test=lambda val: val in str_values
+            )
 
-                    cb = CheckboxInput(final_attrs, check_test=lambda value: value in str_values)
-                    option_value = force_text(option_value)
-                    rendered_cb = cb.render(name, option_value, attrs={
-                        'class': 'check-with-label',
-                        'style': 'display:none;'
-                    })
-                    option_label = force_text(option_label)
-                    output.append(format_html('            <li>{1}<label{0} class="label-for-check">{2}</label></li>',
-                                              label_for, rendered_cb, option_label))
+            value = force_text(value)
+            rendered_cb = cb.render(name, value, attrs={
+                'class': 'check-with-label',
+                'style': 'display:none;'
+            })
+            label = force_text(label)
+            output.append(
+                format_html(
+                    '<li>{1}<label{0} class="label-for-check">{2}</label></li>',
+                    label_for,
+                    rendered_cb,
+                    label
+                )
+            )
 
-                output.append('        </ul>')
-                output.append('    </div>')
+        output.append('</ul>')
         output.append('</div>')
-        return mark_safe('\n'.join(output))
+
+
+class RangeWidget(forms.MultiWidget):
+    def __init__(self, widget, *args, **kwargs):
+        widgets = (widget, widget)
+
+        super(RangeWidget, self).__init__(widgets=widgets, *args, **kwargs)
+
+    def decompress(self, value):
+        return value
+
+    def format_output(self, rendered_widgets):
+        widget_context = {
+            'min': rendered_widgets[0],
+            'max': rendered_widgets[1],
+        }
+        return render_to_string('filter/price.html', widget_context)
+
+
+class RangeField(forms.MultiValueField):
+    default_error_messages = {
+        'invalid_start': _(u'Wprowadź poprawną cenę'),
+        'invalid_end': _(u'Wprowadź poprawną cenę'),
+    }
+
+    def __init__(self, field_class, widget=forms.TextInput, *args, **kwargs):
+        if not 'initial' in kwargs:
+            kwargs['initial'] = ['', '']
+
+        fields = (field_class(), field_class())
+
+        super(RangeField, self).__init__(
+            fields=fields,
+            widget=RangeWidget(widget),
+            *args,
+            **kwargs
+        )
+
+    def compress(self, data_list):
+        if data_list:
+            return [
+                self.fields[0].clean(data_list[0]),
+                self.fields[1].clean(data_list[1]),
+            ]
+
+        return None
 
 
 class SpisTresciSearchForm(ModelSearchForm):
@@ -82,14 +182,13 @@ class SpisTresciSearchForm(ModelSearchForm):
             }
         )
     )
-    price_from = forms.DecimalField(required=False)
-    price_to = forms.DecimalField(required=False)
-    bookstore = forms.ModelMultipleChoiceField(
+    price_range = RangeField(forms.DecimalField)
+    bookstores = forms.ModelMultipleChoiceField(
         queryset=Bookstore.objects.all(),
-        widget=CheckboxSelectMultiple,
+        widget=STCheckboxSelectMultiple,
         required=False,
     )
-    format = forms.ModelMultipleChoiceField(
+    formats = forms.ModelMultipleChoiceField(
         queryset=BookFormat.objects.all(),
         widget=STCheckboxSelectMultiple(
             group_by=BookFormatType,
@@ -101,15 +200,15 @@ class SpisTresciSearchForm(ModelSearchForm):
         # First, store the SearchQuerySet received from other processing.
         sqs = super(SpisTresciSearchForm, self).search()
 
-        if self.cleaned_data['price_from']:
-            sqs = sqs.filter(
-                price_highest__gte=self.cleaned_data['price_from']
-            )
-
-        if self.cleaned_data['price_to']:
-            sqs = sqs.filter(
-                price_lowest__lte=self.cleaned_data['price_to']
-            )
+        # if self.cleaned_data['price_from']:
+        #     sqs = sqs.filter(
+        #         price_highest__gte=self.cleaned_data['price_from']
+        #     )
+        #
+        # if self.cleaned_data['price_to']:
+        #     sqs = sqs.filter(
+        #         price_lowest__lte=self.cleaned_data['price_to']
+        #     )
 
         # if self.cleaned_data['bookstore']:
         #     sqs = sqs.filter(bookstore__in=self.cleaned_data['bookstore'])
